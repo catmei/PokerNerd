fca8a0231b19d79882d208190c65695854a0a652
catvin laptop <b02202040@gmail.com>
2023-09-26 22:17:48 +0800

    first version hand history and save to RDS

--- image_recognition_test/image_recognition.py
+++ image_recognition_test/image_recognition.py
@@ -11,72 +11,93 @@ if __name__ == "__main__":
         config = yaml.safe_load(stream)
 
     # Initialize
-    # image = cv2.imread('images_for_test_2\pot_example_0.png')
-    # image_test = ImageTest(image, config)
-    # image_test.show_image()
+    image = cv2.imread(r'C:\Users\b0220\Documents\data_engineering\PokerNerd\main\images\game1\screenshot_261.png')
+    image_test = ImageTest(image, config)
+    image_test.show_image()
 
     # # is my turn
     # result = image_test.is_main_player_turn()
     # print(f'is my turn?: {result}')
 
-    # detect table cards
+    # # detect turn
+    # players_turn = image_test.detect_players_turn()
+    # print(players_turn)
+
+    # count_white
+    # player_turn = image_test.detect_players_turn_()
+    # print(player_turn)
+
+    # # detect table cards
     # table_cards = image_test.detect_table_cards()
     # print(table_cards)
 
-    # detect my cards
+    # detect action
+    action = image_test.detect_action(player=3)
+    print(action)
+
+    # # detect my cards
     # my_cards = image_test.detect_hero_cards()
     # print(my_cards)
 
-    # find dealer
-    image = cv2.imread('sitting_out.png')
-    image_test = ImageTest(image, config)
-    image_test.show_image()
-    players_info = image_test.get_dealer_button_position()
-    print(players_info)
+    # # find dealer
+    # image = cv2.imread('bet_image/bet_all.png')
+    # image_test = ImageTest(image, config)
+    # image_test.show_image()
+    # players_info = image_test.get_dealer_button_position()
+    # print(players_info)
+    #
+    # # find empty seat
+    # players_info = image_test.get_empty_seats(players_info)
+    # print(players_info)
+    #
+    # # find sitting out players
+    # players_info = image_test.get_so_players(players_info)
+    # print(players_info)
+    #
+    # players_info = image_test.assign_positions(players_info)
+    # print(players_info)
+    #
+    # players_info = image_test.find_players_bet_(players_info)
+    # print(players_info)
+    # exit()
 
-    # find empty seat
-    players_info = image_test.get_empty_seats(players_info)
-    print(players_info)
+    # # find bet by player
+    # for i in range(1,7):
+    #     player_bet = image_test.find_player_bet(player=i)
+    #     print(player_bet)
 
-    # find sitting out players
-    players_info = image_test.get_so_players(players_info)
-    print(players_info)
-
-    players_info = image_test.assign_positions(players_info)
-    print(players_info)
-    exit()
 
     # # detect total pot cards
     # total_pot = image_test.find_total_pot()
     # print(total_pot)
 
-    # test pot
-    dir = 'images_for_test_2'
-
-    for i in range(109):
-        print(i)
-
-        image = cv2.imread(f'{dir}/pot_example_{i}.png')
-        image_test = ImageTest(image, config)
-        image_test.show_image()
-
-        total_pot = image_test.find_total_pot()
-        print(total_pot)
-        print('-'*50)
-
-    exit()
-
-    dir = 'to_be_trained_others'
-
-    all_files_and_directories = os.listdir(dir)
-    print(all_files_and_directories)
-    for i in all_files_and_directories:
-        print(i)
-
-        image = cv2.imread(f'{dir}/{i}')
-        image_test = ImageTest(image, config)
-        image_test.show_image()
-
-        total_pot = image_test.find_total_pot()
-        print(total_pot)
-        print('-'*50)
+    # # test pot
+    # dir = 'images_for_test_2'
+    #
+    # for i in range(109):
+    #     print(i)
+    #
+    #     image = cv2.imread(f'{dir}/pot_example_{i}.png')
+    #     image_test = ImageTest(image, config)
+    #     image_test.show_image()
+    #
+    #     total_pot = image_test.find_total_pot()
+    #     print(total_pot)
+    #     print('-'*50)
+    #
+    # exit()
+    #
+    # dir = 'to_be_trained_others'
+    #
+    # all_files_and_directories = os.listdir(dir)
+    # print(all_files_and_directories)
+    # for i in all_files_and_directories:
+    #     print(i)
+    #
+    #     image = cv2.imread(f'{dir}/{i}')
+    #     image_test = ImageTest(image, config)
+    #     image_test.show_image()
+    #
+    #     total_pot = image_test.find_total_pot()
+    #     print(total_pot)
+    #     print('-'*50)
--- main/main.py
+++ main/main.py
@@ -1,81 +1,233 @@
 import cv2
 import numpy as np
+import pandas as pd
+import json
 import pyautogui
 import pygetwindow
 from pokerstars_recognition import PokerStarsTableRecognizer
-from utils import read_config_file, set_window_size, remove_cards, data_concatenate, resize_window
+from utils import read_config_file, resize_window, save_history
+from strategy import calc_equity, remove_cards
 import time
+from enum import Enum
+import os
+import uuid
 
-config = read_config_file(filename="../config2.yaml")
-resize_window("Hold'em", 2000, 1500)
-table_data = []
-player_position = None
-print_my_turn = 1
-last_pot = None
-last_table_cards = None
-last_my_cards = None
 
-count = 0
-while True:
-    # start = time.time()
-    window = pygetwindow.getWindowsWithTitle("No Limit Hold'em")[0]
-    screenshot = pyautogui.screenshot(region=(window.left, window.top, window.width, window.height))
+class ROUND(Enum):
+    PRE_FLOP = 'Pre-Flop'
+    FLOP = 'Flop'
+    TURN = 'Turn'
+    RIVER = 'River'
 
-    screenshot = np.array(screenshot)
-    screenshot = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
-    recognizer = PokerStarsTableRecognizer(screenshot, config)
 
-    try:
-        # if not player_position:
-        # player_position = recognizer.get_dealer_button_position()
-        # print(player_position)
+class GameRecorder:
+    window_name = "Hold'em"
 
-        is_my_turn = recognizer.is_main_player_turn()
-        # cv2.imshow('Computer Vision', screenshot)
+    def __init__(self, conf_path):
+        self.config = read_config_file(filename=conf_path)
+        self.column_names = ['game_id', 'round', 'player', 'position', 'action', 'number', 'stack_before',
+                             'stack_after', 'pot_before', 'pot_after', 'my_cards', 'table_cards']
+        # 'stack_size', 'my_cards', 'table_cards', 'equity']
+        self.history = pd.DataFrame(columns=self.column_names)
 
-        if is_my_turn:
-            if print_my_turn == 1:
-                print("It's my turn")
-                print_my_turn = 0
+        self.game_id = None
+        self.players_position = None
+        self.round = None
+        self.my_cards = []
+        self.start_strategy = 0
+
+        self.last_pot = None
+        self.last_players_bet = None
+        self.last_players_turn = None
+        self.last_table_cards = []
+        self.action_record_queue = []
+        self.bet_record_queue = []
+
+    def reset_game(self):
+        pass
+
+    def recognize_image(self, recognizer):
+        try:
+            table_cards = recognizer.detect_table_cards()
+            pot = recognizer.find_total_pot()
+
+            if len(self.last_table_cards) == 0:
+                self.last_table_cards = table_cards
+
+            # define round
+            if table_cards != self.last_table_cards:
+                if len(table_cards) == 0 and len(self.last_table_cards) == 5:
+                    # save and clean last game history
+                    if self.game_id:
+                        print(self.history)
+                        self.history['my_cards'] = self.history['my_cards'].apply(json.dumps)
+                        self.history['table_cards'] = self.history['table_cards'].apply(json.dumps)
+                        save_history(df_history=self.history)
+                        self.history = pd.DataFrame(columns=self.column_names)
+
+                    print('=' * 50)
+                    print('New Game Starts!')
+                    # New Game Starts
+                    self.game_id = None
+                    self.players_position = None
+                    self.round = None
+                    self.last_pot = None
+                    self.last_players_bet = None
+                    self.last_players_turn = None
+
+                    # start new game
+                    self.game_id = int(time.time())
+                    self.round = ROUND.PRE_FLOP
+
+                    players_info = recognizer.get_dealer_button_position()
+                    players_info = recognizer.get_empty_seats(players_info)
+                    players_info = recognizer.get_so_players(players_info)
+                    self.players_position = recognizer.assign_positions(players_info)
+
+                    print(f'Game ID: {self.game_id}')
+                    print(f'Players Position: {self.players_position}')
+                    print('-' * 50)
+                    print(f'Round: {self.round.value}')
+                    print(f'table cards: {table_cards}')
+
+                elif len(table_cards) == 3 and len(self.last_table_cards) == 0:
+                    # clean pre-flop players turn (BB or SB would repeat)
+                    self.last_players_turn = 0
+                    self.round = ROUND.FLOP
+                    print('-' * 50)
+                    print(f'Round: {self.round.value}')
+                    print(f'table cards: {table_cards}')
+
+                elif len(table_cards) == 4 and len(self.last_table_cards) == 3:
+                    self.round = ROUND.TURN
+                    print('-' * 50)
+                    print(f'Round: {self.round.value}')
+                    print(f'table cards: {table_cards}')
+
+                elif len(table_cards) == 5 and len(self.last_table_cards) == 4:
+                    self.round = ROUND.RIVER
+                    print('-' * 50)
+                    print(f'Round: {self.round.value}')
+                    print(f'table cards: {table_cards}')
+
+                self.last_table_cards = table_cards
+
+            else:
+                pass
+
+            # check recording from the pre-flop instead of middle
+            if self.players_position:
+                players_turn = recognizer.detect_players_turn_()
+
+                # strategy
+                if players_turn == 1:
+                    # record my cards
+                    if len(self.my_cards) == 0:
+                        my_cards = recognizer.detect_hero_cards()
+                        if len(my_cards) == 2:
+                            print(f'My Cards: {my_cards}')
+                            self.my_cards = my_cards
+
+                    # trigger strategy
+                    if len(self.my_cards) > 0 and self.start_strategy == 0:
+                        self.start_strategy = 1
+                        hero_cards = self.my_cards
+                        table_cards = table_cards
+                        deck = remove_cards(hero_cards, table_cards)
+                        equity = calc_equity(deck, hero_cards, table_cards)
+                        print(f'Equity: {equity}')
+                        optimal_bet_amount = int(pot) * equity / (100 - 2 * equity)
+                        print(f'Max Bet Amount: {optimal_bet_amount}')
+
+                if self.start_strategy == 1 and players_turn != 1:
+                    self.start_strategy = 0
+
+                if players_turn != self.last_players_turn and players_turn != 0:
+                    pot = int(pot) if pot else pot
+                    self.action_record_queue.append({
+                        'round': self.round.value,
+                        'player': players_turn,
+                        'pot_before': pot,
+                    })
+                    # print(f'turn: {players_turn}')
+                    # print(f'action queue: {self.action_record_queue}')
+                    self.last_players_turn = players_turn
+
+                if len(self.action_record_queue) > 0:
+                    action_job = self.action_record_queue[0]
+                    player = action_job['player']
+                    action = recognizer.detect_action(player=player)
+                    if action:
+                        action_job['action'] = action
+                        self.bet_record_queue.append(action_job)
+                        # print(f'player_{player} action: {action}')
+                        # print(f'bet queue: {self.bet_record_queue}')
+                        del self.action_record_queue[0]
+
+                if len(self.bet_record_queue) > 0:
+                    bet_job = self.bet_record_queue[0]
+                    player = bet_job['player']
+                    number = recognizer.find_player_bet(player=player)
+                    number = int(number) if number else number
+
+                    pot = int(pot) if pot else pot
+
+                    bet_job['number'] = number
+                    bet_job['pot_after'] = pot
+                    record = bet_job
+                    print(record)
+
+                    record['game_id'] = self.game_id
+                    record['my_cards'] = [self.my_cards]
+                    record['table_cards'] = [table_cards]
+                    record['position'] = 'test'
+                    record['stack_before'] = 0
+                    record['stack_after'] = 0
+                    new_row = pd.DataFrame(record, index=[0])
+                    self.history = pd.concat([self.history, new_row], ignore_index=True)
+
+                    del self.bet_record_queue[0]
+
+        except Exception as Error:
+            raise
+            # print(Error)
+
+    def run(self, source, debug=False, save=False):
+        count = 0
+        if source == 'live':
+            while True:
+                resize_window(self.window_name, 2000, 1500)
+                window = pygetwindow.getWindowsWithTitle(self.window_name)[0]
+                screenshot = pyautogui.screenshot(region=(window.left, window.top, window.width, window.height))
+                if save:
+                    screenshot.save(f'images/screenshot_{count}.png')
+                count += 1
+                screenshot = np.array(screenshot)
+                screenshot = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
+                recognizer = PokerStarsTableRecognizer(screenshot, self.config)
+                self.recognize_image(recognizer)
         else:
-            print_my_turn = 1
 
-        # detect table cards
-        new_table_cards = recognizer.detect_table_cards()
-        if new_table_cards != last_table_cards:
+            filenames = os.listdir(source)
+            sorted_filenames = sorted(filenames, key=lambda x: int(x.split('_')[1].split('.png')[0]))
 
-            players_info = recognizer.get_dealer_button_position()
-            players_info = recognizer.get_empty_seats(players_info)
-            players_info = recognizer.get_so_players(players_info)
-            players_info = recognizer.assign_positions(players_info)
-            print(players_info)
+            for file in sorted_filenames:
+                path = os.path.join(source, file)
+                screenshot = cv2.imread(path)
 
-            print(f'table_cards: {new_table_cards}')
-            last_table_cards = new_table_cards
+                if debug:
+                    cv2.imshow('Screenshot', screenshot)
+                    key = cv2.waitKey(0)
+                    if key == ord('q'):
+                        break
+                    cv2.destroyAllWindows()
 
-        # detect my cards
-        new_my_cards = recognizer.detect_hero_cards()
-        if new_my_cards != last_my_cards:
-            print(f'my_cards: {new_my_cards}')
-            last_my_cards = new_my_cards
+                recognizer = PokerStarsTableRecognizer(screenshot, self.config)
+                self.recognize_image(recognizer)
 
-        new_pot, boxes_number = recognizer.find_total_pot()
-        if new_pot == last_pot:
-            pass
-        else:
-            screenshot = pyautogui.screenshot(region=(window.left, window.top, window.width, window.height))
-            # screenshot.save(f'pot_example_{count}.png')
 
-            # print('-' * 50)
-            # print(f"png_{count}")
-            print(f"pot: {new_pot}")
-            # print(f"boxes number: {boxes_number}")
-            # print('-' * 50)
-
-            last_pot = new_pot
-            count += 1
-    except Exception as Error:
-        print(Error)
-
-    # end = time.time()
-    # print(f'fps: {1 / (end - start) }')
+game_recorder = GameRecorder(conf_path='../config2.yaml')
+# game_recorder.run(source='live', save=True)
+start = time.time()
+game_recorder.run(source='images/my_hand_history/game1', debug=False)
+# print(time.time() - start)
